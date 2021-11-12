aa20c9c8a3c360bdfadb6494057c42bff80c1768
tfarley <tfarleygithub@gmail.com>
2021-11-11 21:47:17 -0800

    Update .NET from 5.0 to 6.0 Update packages Add note for how to build PdbTool successfully

--- src/AC2RE.PdbTool/Program.cs
+++ src/AC2RE.PdbTool/Program.cs
@@ -2,6 +2,7 @@
 
 namespace AC2RE.PdbTool {
 
+    // NOTE: In order for build to succeed, run as Admin: regsvr32 "C:\Program Files\Microsoft Visual Studio\2022\Community\DIA SDK\bin\amd64\msdia140.dll"
     public class Program {
 
         public static void Main(string[] args) {
